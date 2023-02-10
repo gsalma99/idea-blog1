a074ecf9323c1253f3b846d26e95953aea01c989
gsalma99 <salmagonzalezzz10011@gmail.com>
2023-02-10 13:37:39 -0600

    working on debugging

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,25 @@
 const handleLogin = async event => {
-    event.preventDefault();
-    
-    const username = document.querySelector('#username-input-login').value;
-    const password = document.querySelector('#password-input-login').value;
+  event.preventDefault();
   
-    try {
-      const res = await fetch('/api/user/login', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (!res.ok) {
-        throw new Error(res.statusText);
-      }
-  
-      document.location.replace('/dashboard');
-    } catch (error) {
-      console.error(error);
-      alert('Failed to login');
+  const username = document.querySelector('#username-input-login').value;
+  const password = document.querySelector('#password-input-login').value;
+
+  try {
+    const res = await fetch('/api/user/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (!res.ok) {
+      throw new Error(res.statusText);
     }
-  };
-  
-  document.querySelector('#login-form').addEventListener('submit', handleLogin);
+
+    document.location.replace('/dashboard');
+  } catch (error) {
+    console.error(error);
+    alert('Failed to login');
+  }
+};
+
+document.querySelector('#login-form').addEventListener('submit', handleLogin);
